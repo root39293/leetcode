5c41f5686af14330320cf998660f7806984f6940
faith6 <72300594+root39293@users.noreply.github.com>
2024-05-22 17:13:24 +0900

    Time: 25 ms (97.13%), Space: 16.6 MB (10.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,8 +1,8 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        try:
+        try: 
             i = haystack.index(needle) #.index() 활용
-        except ValueError:
+        except ValueError: # 문자열이 존재하지않을경우 ValueError 예외가 발생하므로 -1반환 
             return -1
         return i
             
